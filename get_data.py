089f197bf58e6558adc9023575ead8b7bcb73a61
Clint Olson <coandco@gmail.com>
2023-11-30 16:24:12 -0800

    Add scaffolding

---
+++ get_data.py
@@ -0,0 +1,44 @@
+import aocd.exceptions
+from aocd.models import Puzzle
+from pathlib import Path
+import re
+import sys
+
+DIGITS = re.compile(r'\d+')
+
+# This is a standalone script meant to be run to automatically grab my data
+# for a given year/day and dump it into an automatically-named file.
+
+YEAR_NUM = 2023
+
+existing_inputs = list(Path('inputs/').glob('*.txt'))
+old_max_day = max(int(DIGITS.findall(x)[1]) for x in existing_inputs) if existing_inputs else 0
+new_day = old_max_day + 1
+
+try:
+    puzzle = Puzzle(year=YEAR_NUM, day=new_day)
+
+    file_location = Path(f'inputs/advent{YEAR_NUM}_day{new_day:02d}_input.txt')
+    file_location.write_text(puzzle.input_data)
+except aocd.exceptions.PuzzleLockedError as e:
+    print(f"Day {new_day} not available: {e}")
+    sys.exit(1)
+
+
+program_location = Path(f"advent{YEAR_NUM}_day{new_day:02d}.py")
+blank_day = """from utils import read_data
+import time
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    start = time.monotonic()
+    main()
+    print(f"Time: {time.monotonic()-start}")
+"""
+
+if not program_location.exists():
+    program_location.write_text(blank_day)
