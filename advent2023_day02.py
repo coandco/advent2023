d9e129bf604c045ea9608e1307f26bd75694e336
Clint Olson <coandco@gmail.com>
2023-12-02 11:37:27 -0800

    Clean up day 2

--- advent2023_day02.py
+++ advent2023_day02.py
@@ -1,36 +1,32 @@
-from typing import List, NamedTuple
-from utils import read_data
+import re
 import time
+from collections import defaultdict
+from math import prod
 
+from utils import read_data
 
-class Round(NamedTuple):
-    red: int = 0
-    blue: int = 0
-    green: int = 0
-
-    @classmethod
-    def from_string(cls, raw_round: str) -> 'Round':
-        seen = {}
-        for num_and_color in raw_round.split(", "):
-            num, color = num_and_color.split(" ")
-            seen[color] = int(num)
-        return cls(**seen)
+PILES = re.compile(r"(?P<amount>\d+) (?P<color>red|green|blue)")
 
 
 class Game:
-    game_num: int
-    rounds: List[Round]
+    max_colors: defaultdict[str, int]
 
     def __init__(self, line: str):
-        gamenum_str, games_recs = line.split(": ", maxsplit=1)
-        self.game_num = gamenum_str.split(" ", maxsplit=1)[1]
-        self.rounds = [Round.from_string(x) for x in games_recs.split("; ")]
+        self.max_colors = defaultdict(int)
+        for match in PILES.finditer(line):
+            self.max_colors[match.group("color")] = max(
+                self.max_colors[match.group("color")], int(match.group("amount"))
+            )
 
     def is_valid(self, max_red: int, max_green: int, max_blue: int) -> bool:
-        return all(x.red <= max_red and x.blue <= max_blue and x.green <= max_green for x in self.rounds)
+        return (
+            self.max_colors["red"] <= max_red
+            and self.max_colors["green"] <= max_green
+            and self.max_colors["blue"] <= max_blue
+        )
 
     def get_power(self) -> int:
-        return max(x.red for x in self.rounds) * max(x.blue for x in self.rounds) * max(x.green for x in self.rounds)
+        return prod(self.max_colors.values())
 
 
 def main():
@@ -39,7 +35,7 @@ def main():
     print(f"Part two: {sum(x.get_power() for x in games)}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start = time.monotonic()
     main()
     print(f"Time: {time.monotonic()-start}")
