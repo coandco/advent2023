b028a9371af03328f3f34fef891868dae5be7aed
Clint Olson <coandco@gmail.com>
2023-12-08 21:55:48 -0800

    Update day 9 with cleaner solution (thanks Caleb!)

--- advent2023_day09.py
+++ advent2023_day09.py
@@ -1,4 +1,3 @@
-from collections import deque
 from typing import List
 
 from utils import read_data
@@ -11,28 +10,24 @@ class Sequence:
     def __init__(self, line: str):
         self.sequence = [int(x) for x in line.split()]
 
-    def predict(self, time_travel=False):
-        derivatives = [deque(self.sequence)]
+    def predict(self, reversed=False):
+        derivatives = [self.sequence[::-1]] if reversed else [self.sequence]
         while not all(x == 0 for x in derivatives[-1]):
             current = derivatives[-1]
-            derivative = deque(current[i+1] - current[i] for i in range(len(current) - 1))
-            derivatives.append(derivative)
+            derivatives.append([current[i+1] - current[i] for i in range(len(current) - 1)])
         previous = derivatives.pop()
         while derivatives:
             current = derivatives[-1]
-            if time_travel:
-                current.appendleft(current[0] - previous[0])
-            else:
-                current.append(current[-1] + previous[-1])
+            current.append(current[-1] + previous[-1])
             previous = derivatives.pop()
-        return previous[0] if time_travel else previous[-1]
+        return previous[-1]
 
 
 def main():
     sequences = [Sequence(x) for x in read_data().splitlines()]
     predictions = [x.predict() for x in sequences]
     print(f"Part one: {sum(predictions)}")
-    backwards_predictions = [x.predict(time_travel=True) for x in sequences]
+    backwards_predictions = [x.predict(reversed=True) for x in sequences]
     print(f"Part two: {sum(backwards_predictions)}")
 
 
