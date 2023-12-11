f8e41d7e5cb4f3f4418e6e8743d87a3ccc6abd86
Clint Olson <coandco@gmail.com>
2023-12-10 20:44:17 -0800

    Day 10 part 2

--- advent2023_day10.py
+++ advent2023_day10.py
@@ -1,14 +1,21 @@
-from typing import FrozenSet, Dict, List, Tuple
+from typing import Dict, Union, Set
 
-from utils import read_data, BaseCoord as Coord, CARDINAL_NEIGHBORS_2D
+from utils import read_data, BaseCoord as Coord
 import time
 
 REPLACEMENTS = {"F": "┌", "7": "┐", "J": "┘", "L": "└"}
+DIRECTIONS = {"N": Coord(x=0, y=-1), "E": Coord(x=1, y=0), "S": Coord(x=0, y=1), "W": Coord(x=-1, y=0)}
+REAL_START_CHAR = {("N", "E"): "└", ("E", "S"): "┌", ("S", "W"): "┐", ("W", "N"): "┘"}
+TURNS = {
+    "R": {"N": "E", "E": "S", "S": "W", "W": "N"},
+    "L": {"N": "W", "W": "S", "S": "E", "E": "N"},
+    "S": {"N": "N", "E": "E", "S": "S", "W": "W"},
+}
 CONNECTIONS = {
-    Coord(x=1, y=0): {"┐": Coord(x=0, y=1), "┘": Coord(x=0, y=-1), "-": Coord(x=1, y=0)},
-    Coord(x=-1, y=0): {"┌": Coord(x=0, y=1), "└": Coord(x=0, y=-1), "-": Coord(x=-1, y=0)},
-    Coord(x=0, y=1): {"┘": Coord(x=-1, y=0), "└": Coord(x=1, y=0), "|": Coord(x=0, y=1)},
-    Coord(x=0, y=-1): {"┌": Coord(x=1, y=0), "┐": Coord(x=-1, y=0), "|": Coord(x=0, y=-1)},
+    "N": {"┌": "R", "┐": "L", "|": "S"},
+    "E": {"┐": "R", "┘": "L", "-": "S"},
+    "S": {"┘": "R", "└": "L", "|": "S"},
+    "W": {"└": "R", "┌": "L", "-": "S"},
 }
 
 
@@ -26,42 +33,82 @@ class PipeDream:
                     self.start_loc = curloc
                 self.points[Coord(y=y, x=x)] = char
 
-    def __str__(self, highlight: Coord = Coord(x=10000, y=10000)) -> str:
+    def __str__(self, highlight: Union[Coord, Set[Coord], None] = None) -> str:
+        if highlight is None:
+            highlight = set()
+        if isinstance(highlight, Coord):
+            highlight = {highlight}
+
         output = []
         for y in range(max(x.y for x in self.points) + 1):
             line = "".join(
-                "X" if Coord(y=y, x=x) == highlight else self.points.get(Coord(y=y, x=x), " ")
+                "X" if Coord(y=y, x=x) in highlight else self.points.get(Coord(y=y, x=x), ".")
                 for x in range(max(x.x for x in self.points) + 1)
             )
             output.append(line)
         return "\n".join(output)
 
-    def print_field(self):
-        print(self.field_as_str())
-
     def winnow(self) -> int:
-        main_loop = {self.start_loc: self.points[self.start_loc]}
-        steps = [self.start_loc]
-
-        for heading in CARDINAL_NEIGHBORS_2D:
-            if self.points.get(steps[-1] + heading, '.') in CONNECTIONS[heading]:
-                steps.append(steps[-1] + heading)
-                break
-        else:
-            raise Exception("No valid connections found from starting loc")
-        while steps[-1] != self.start_loc:
+        curloc = self.start_loc
+        valid_headings = [x for x in DIRECTIONS if self.points.get(curloc + DIRECTIONS[x], ".") in CONNECTIONS[x]]
+        assert len(valid_headings) == 2, f"Invalid number of connections found from starting loc"
+        main_loop = {curloc: REAL_START_CHAR[tuple(valid_headings)]}
+        heading = valid_headings[0]
+        curloc += DIRECTIONS[heading]
+        while curloc != self.start_loc:
             # When we're following the main loop, we should always have a valid connection
-            assert self.points[steps[-1]] in CONNECTIONS[heading]
-            main_loop[steps[-1]] = self.points[steps[-1]]
-            heading = CONNECTIONS[heading][self.points[steps[-1]]]
-            steps.append(steps[-1] + heading)
+            assert self.points[curloc] in CONNECTIONS[heading]
+            main_loop[curloc] = self.points[curloc]
+            turn_direction = CONNECTIONS[heading][self.points[curloc]]
+            heading = TURNS[turn_direction][heading]
+            curloc += DIRECTIONS[heading]
         self.points = main_loop
-        return len(steps) // 2
+        return len(main_loop) // 2
+
+    def num_enclosed(self) -> int:
+        # find_enclosed() assumes self.winnow() has already been run
+        enclosed_points = set()
+
+        # Start with a westmost pipe heading north.  By definition, the inside is to the east.
+        min_x = min(x.x for x in self.points)
+        start = curloc = next(x for x in self.points if x.x == min_x and self.points[x] != "└")
+        heading = "N"
+        inner_heading = "E"
+
+        # Manually process the start point we picked, checking it for an adjacent interior square
+        if curloc + DIRECTIONS[inner_heading] not in self.points:
+            enclosed_points.add(curloc + DIRECTIONS[inner_heading])
+        turn_direction = CONNECTIONS[heading][self.points[curloc]]
+        heading = TURNS[turn_direction][heading]
+        inner_heading = TURNS[turn_direction][inner_heading]
+        curloc += DIRECTIONS[heading]
+        # Go around the loop, adding any non-occupied squares in the inside direction to our set of enclosed points
+        while curloc != start:
+            if curloc + DIRECTIONS[inner_heading] not in self.points:
+                enclosed_points.add(curloc + DIRECTIONS[inner_heading])
+            turn_direction = CONNECTIONS[heading][self.points[curloc]]
+            heading = TURNS[turn_direction][heading]
+            inner_heading = TURNS[turn_direction][inner_heading]
+            # When we go around a corner, we need to check both inside neighbors
+            if curloc + DIRECTIONS[inner_heading] not in self.points:
+                enclosed_points.add(curloc + DIRECTIONS[inner_heading])
+            curloc += DIRECTIONS[heading]
+
+        # At this point, we've marked all enclosed points next to walls, so we just need to flood fill
+        queue = list(enclosed_points)
+        while queue:
+            curloc = queue.pop()
+            for neighbor in curloc.cardinal_neighbors():
+                if not any(neighbor in x for x in (enclosed_points, self.points)):
+                    enclosed_points.add(neighbor)
+                    queue.append(neighbor)
+        return len(enclosed_points)
 
 
 def main():
     field = PipeDream(read_data())
     print(f"Part one: {field.winnow()}")
+    print(f"Part two: {field.num_enclosed()}")
 
 
 if __name__ == "__main__":
