14c269ae25e4baded4280471940174a5756988df
Clint Olson <coandco@gmail.com>
2023-12-17 19:30:18 -0800

    Day 17 black/isort pass

--- advent2023_day17.py
+++ advent2023_day17.py
@@ -1,16 +1,14 @@
 import heapq
-import sys
 import time
-from collections import deque, defaultdict
-from typing import List, Optional, Dict, Union, Set, Iterable
+from typing import Dict
 
 from utils import BaseCoord as Coord
 from utils import read_data
 
 DIRECTIONS = {"N": Coord(x=0, y=-1), "E": Coord(x=1, y=0), "S": Coord(x=0, y=1), "W": Coord(x=-1, y=0)}
-RIGHT_TURNS = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
-LEFT_TURNS = {'N': 'W', 'E': 'N', 'S': 'E', 'W': 'S'}
-VALID_DIRECTIONS = {'N': {'N', 'W', 'E'}, 'E': {'E', 'N', 'S'}, 'S': {'S', 'W', 'E'}, 'W': {'W', 'N', 'S'}}
+RIGHT_TURNS = {"N": "E", "E": "S", "S": "W", "W": "N"}
+LEFT_TURNS = {"N": "W", "E": "N", "S": "E", "W": "S"}
+VALID_DIRECTIONS = {"N": {"N", "W", "E"}, "E": {"E", "N", "S"}, "S": {"S", "W", "E"}, "W": {"W", "N", "S"}}
 
 
 class HeatMap:
@@ -32,9 +30,9 @@ class HeatMap:
         # We need to avoid loops
         seen = set()
         heap = []
-        destination = Coord(y=self.max_y-1, x=self.max_x-1)
+        destination = Coord(y=self.max_y - 1, x=self.max_x - 1)
         # heat_loss, curloc, last_dir, run_length
-        heapq.heappush(heap, (0, Coord(0, 0), 'E', 0))
+        heapq.heappush(heap, (0, Coord(0, 0), "E", 0))
         while heap:
             heat_loss, curloc, last_dir, run_length = heapq.heappop(heap)
             if (curloc, last_dir, run_length) in seen:
